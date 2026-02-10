dda3b5d4593e089b08f765c42a075d4092f7c486
Sarthak Srivastava <sarthakmahi345@gmail.com>
2026-02-10 18:56:40 +0530

    Update admin.js

--- admin.js
+++ admin.js
@@ -598,6 +598,59 @@ function loadProducts(searchQuery = '') {
     `;
   });
 }
+//===================================//
+//======UPLOADIMAGE==================//
+
+async function uploadImage() {
+  const fileInput = document.getElementById("pImage");
+  const file = fileInput.files[0];
+
+  if (!file) {
+    alert("Please select an image first");
+    return;
+  }
+
+  const formData = new FormData();
+  formData.append("file", file);
+  formData.append("upload_preset", "unsigned_preset"); // apna preset name
+
+  try {
+    const res = await fetch("https://api.cloudinary.com/v1_1/YOUR_CLOUD_NAME/image/upload", {
+      method: "POST",
+      body: formData
+    });
+
+    const data = await res.json();
+    uploadedImageUrl = data.secure_url;
+
+    alert("Image uploaded successfully");
+  } catch (err) {
+    console.error(err);
+    alert("Image upload failed");
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 /* ===============================
    ANALYTICS & CHARTS
