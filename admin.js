75833123fc6e2b13a600bda5549a58ca2894e407
Sarthak Srivastava <sarthakmahi345@gmail.com>
2026-02-22 23:50:41 +0530

    Update admin.js

--- admin.js
+++ admin.js
@@ -841,7 +841,7 @@ async function loadOrders() {
             <p><strong>Name:</strong> ${order.customerName || "-"}</p>
                <p><strong>Phone:</strong> ${order.phone || "-"}</p>
 
-            <p><strong>Total:</strong> ${Number(order.total) || 0}</p>
+            <p><strong>Total:</strong> ${Number(order.totalAmount) || 0}</p>
 
         <p><strong>Items:</strong> ${
           order.items ? order.items.length : 0
